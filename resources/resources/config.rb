f654c608ea9ccd0d94018828b79fc7cded477194
Javier Rodriguez Gomez <javiercrg@redborder.com>
2022-05-04 14:44:55 +0100

    Added proxy mode behavior

--- resources/resources/config.rb
+++ resources/resources/config.rb
@@ -20,6 +20,7 @@ attribute :memcached_server, :kind_of => String, :default => "memcached.service"
 attribute :mac_vendors, :kind_of => String, :default => "/etc/objects/mac_vendors"
 attribute :mongo_cve_database, :kind_of => String, :default => 'cvedb'
 attribute :mongo_port, :kind_of => String, :default => '27017'
+attribute :mode, :kind_of => String, :default => 'manager'
 
 # EXAMPLES
 
