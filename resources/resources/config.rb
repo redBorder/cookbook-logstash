b98b66e559b76e71b8f71b1a088d0288cb64ade2
nilsver <139124949+nilsver@users.noreply.github.com>
2025-09-18 11:45:10 +0100

    flow nodes splitted when from proxy (#91)
    * flow nodes splitted when from proxy
    * avoid double enrichment
    * add check to avoid double enrichment

--- resources/resources/config.rb
+++ resources/resources/config.rb
@@ -24,6 +24,8 @@ attribute :split_traffic_logstash, kind_of: [TrueClass, FalseClass], default: fa
 attribute :split_intrusion_logstash, kind_of: [TrueClass, FalseClass], default: false
 attribute :intrusion_incidents_priority_filter, kind_of: String, default: 'high'
 attribute :vault_incidents_priority_filter, kind_of: String, default: 'error'
+attribute :flow_nodes_without_proxy, kind_of: Array, default: []
+attribute :flow_nodes_with_proxy, kind_of: Array, default: []
 attribute :redis_hosts, kind_of: Array, default: []
 attribute :redis_port, kind_of: Integer, default: 26379
 attribute :redis_secrets, kind_of: Hash, default: {}
