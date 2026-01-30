b1deebdcc025f6ff66a7819fdd3c9f29fce152c3
manegron <manegron@redborder.com>
2026-01-30 13:22:13 +0000

    Add rescue

--- resources/libraries/helper.rb
+++ resources/libraries/helper.rb
@@ -4,7 +4,12 @@ module Logstash
       sensors = []
       managers_keys = Chef::Node.list.keys.sort
       managers_keys.each do |m_key|
-        m = Chef::Node.load m_key
+        m = nil
+        begin
+          m = Chef::Node.load m_key
+        rescue
+          Chef::Log.error("[get_managers] Failed to load node: #{m_key}")
+        end
         m = node if m.name == node.name
 
         begin
@@ -50,7 +55,12 @@ module Logstash
       sensors = []
       managers_keys = Chef::Node.list.keys.sort
       managers_keys.each do |m_key|
-        m = Chef::Node.load m_key
+        m = nil
+        begin
+          m = Chef::Node.load m_key
+        rescue
+          Chef::Log.error("[get_sensors_info] Failed to load node: #{m_key}")
+        end
         m = node if m.name == node.name
 
         begin
