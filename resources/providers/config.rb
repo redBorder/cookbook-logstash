91c6c375161e56b5e1f024f0356ebc1ba8cbd4b7
ljblancoredborder <ljblanco@redborder.com>
2026-03-04 12:25:13 +0000

    remove flow_nodes as not longer necessary

--- resources/providers/config.rb
+++ resources/providers/config.rb
@@ -349,7 +349,7 @@ action :add do
         mode '0644'
         ignore_failure true
         cookbook 'logstash'
-        variables(flow_nodes: flow_nodes, proxy_nodes: proxy_nodes, split_traffic_logstash: split_traffic_logstash)
+        variables(proxy_nodes: proxy_nodes, split_traffic_logstash: split_traffic_logstash)
         notifies :restart, 'service[logstash]', :delayed unless node['redborder']['leader_configuring']
       end
 
