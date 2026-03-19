21e1db978c515dfa7cd97c2dd93b59fc93db6ebe
ljblancoredborder <ljblanco@redborder.com>
2026-03-19 12:58:56 +0000

    passing variable if needed

--- resources/providers/config.rb
+++ resources/providers/config.rb
@@ -367,7 +367,8 @@ action :add do
         mode '0644'
         ignore_failure true
         cookbook 'logstash'
-        variables(proxy_nodes: proxy_nodes, split_traffic_logstash: split_traffic_logstash)
+        variables(flow_nodes: sflow_nodes, proxy_nodes: proxy_nodes, split_traffic_logstash: split_traffic_logstash)
+        # variables(proxy_nodes: proxy_nodes, split_traffic_logstash: split_traffic_logstash)
         notifies :restart, 'service[logstash]', :delayed unless node['redborder']['leader_configuring']
       end
 
